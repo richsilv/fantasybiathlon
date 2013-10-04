520a3cfc0d6fce56b9e1784a90cbcfb905d6ece5
Richard Silverton <richardlsilverton32@googlemail.com>
2013-10-04 12:10:57 +0100

    started building

--- fantasybiathlon.js
+++ fantasybiathlon.js
@@ -1,16 +1,33 @@
-FantasyTeam = new Meteor.Collection("fantasyteam");
-
+var FantasyTeams = new Meteor.Collection("fantasyteams");
+var Athletes = new Meteor.Collection("athletes");
+var userid = 1;
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to fantasybiathlon.";
+    console.log(userid);
+    console.log(Athletes.findOne({Nat: "NOR"}));
+    console.log(FantasyTeams.findOne({UserID: userid}));
+    Template.teamname.team = function() {
+	console.log(userid);
+	console.log(FantasyTeams.findOne({UserID: userid}));
+	return FantasyTeams.findOne({UserID: userid});
+    };
+    Template.team.teamdeets = function() {
+	return FantasyTeams.findOne({UserID: userid});
+    };
+    Template.team.athletes = function() {
+	return FantasyTeams.findOne({UserID: userid}, function(err, thisteam) {
+	    return getathletes(thisteam.Athletes);
+	});
+    };
+    Template.hello.greeting = function () {
+	return "Welcome to fantasybiathlon.";
   };
 
   Template.hello.events({
     'click input' : function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+	console.log("You pressed the button");
     }
   });
 }
@@ -19,4 +36,12 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
+
+function getathletes(ibuarray) {
+    var athletearray = [];
+    for (var i = 0; i < ibuarray.length; i++) {
+	athletearray.push(Athletes.findOne({IBUId: ibuarray[i]}));
+    }
+    return athletearray;
 }
